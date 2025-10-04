48687cd1162c0da0f76d4700b45a082a4105d5f8
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-10-04 20:49:50 +0900

    クライアント間の同期処理の実装を行いました。 それに伴い、protoファイルの実装を変更しました。

--- ApplyProto.ps1
+++ ApplyProto.ps1
@@ -14,16 +14,33 @@ $rustOut = "./PortfolioGameServer/game/project/src/proto"
 $protoFiles = @("types.proto", "math.proto")
 
 # C++ 用コード生成
+Write-Host "Running protoc for C++..."
 & $protoc `
     --proto_path=$protoDir `
     --cpp_out=$cppOut `
     ($protoFiles | ForEach-Object { Join-Path $protoDir $_ })
+if ($LASTEXITCODE -ne 0) {
+	# C++コード生成に失敗
+	Write-Host "protoc failed with exit code $LASTEXITCODE"
+	exit $LASTEXITCODE
+}
+Write-Host "Completed."
 
 # Rust 用コード生成
+Write-Host "Running protoc for Rust..."
 & $protoc `
     --proto_path=$protoDir `
     --rust_out=$rustOut `
     --rust_opt=experimental-codegen=enabled,kernel=upb `
     ($protoFiles | ForEach-Object { Join-Path $protoDir $_ })
+if ($LASTEXITCODE -ne 0) {
+	# Rustコード生成に失敗
+	Write-Host "protoc failed with exit code $LASTEXITCODE"
+	exit $LASTEXITCODE
+}
+
+Write-Host "Completed."
+
+Write-Host "Completed protoc."
 
 pause
